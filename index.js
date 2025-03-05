78e69bd81c95bb7b25d4d0f553f872456991b38e
sankalp gupta <guptasankalp2004@gmail.com>
2025-03-05 22:27:27 +0530

    repo initialisation

---
+++ index.js
@@ -0,0 +1,17 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
